6b10c2530c76340e34ab4f1e5ebc186d71cbb23a
Kendrick White <kendrickutwhite@gmail.com>
2026-01-16 17:08:26 -0600

    Modify ddl_bronze.sql to add columns and tables
    Added new columns to crm_cst_info and created new tables for sales details, product info, customer data, ERP locations, and categories.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -8,13 +8,16 @@ CREATE TABLE Bronze.crm_cst_info (
     cst_lastname NVARCHAR(50)
 
 )
+GO
 ALTER TABLE Bronze.crm_cst_info
 ADD cst_material_status NVARCHAR(50),
     cst_gender NVARCHAR(50),
     cst_create_date DATE
 
+GO
 IF OBJECT_ID ('Bronze.crm_sales_details','U')
     DROP TABLE Bronze.crm_sales_details;
+GO
 CREATE TABLE Bronze.crm_sales_details(
     sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
@@ -28,9 +31,12 @@ CREATE TABLE Bronze.crm_sales_details(
 
 
 );
-
+GO
+    
 IF OBJECT_ID ('Bronze.prd_info','U')
     DROP TABLE Bronze.prd_info;
+GO
+    
 CREATE TABLE Bronze.prd_info(
 prd_id NVARCHAR(50),
 prd_key NVARCHAR(50),
@@ -41,93 +47,34 @@ prd_start_dt DATE,
 prd_end_dt DATE
 
 )
-
+GO
+    
 IF OBJECT_ID ('Bronze.cust_az12','U')
     DROP TABLE Bronze.cust_az12;
+GO
 CREATE TABLE Bronze.cust_az12 (
     cid   NVARCHAR(50),
     bdate DATE,
     gen   NVARCHAR(10)
 );
-
+GO
+    
 IF OBJECT_ID ('Bronze.erp_loc_a101','U')
     DROP TABLE Bronze.erp_loc_a101;
+GO
 CREATE TABLE Bronze.erp_loc_a101 (
     cid   NVARCHAR(50),
     entry NVARCHAR(50)
 );
-
+GO
+    
 IF OBJECT_ID ('Bronze.erp_px_cat_g1v2','U') IS NOT NULL
     DROP TABLE Bronze.erp_px_cat_g1v2
+GO
 CREATE TABLE Bronze.erp_px_cat_g1v2(
     id NVARCHAR(50),
     cat NVARCHAR(50),
     subcat NVARCHAR(50),
     maintenance NVARCHAR(50)
 );
-
-CREATE OR ALTER PROCEDURE Bronze.load_bronze AS
-    BEGIN
-        DECLARE @startTime DATETIME, @endtime DATETIME,
-            @batchStartTime DATETIME, @batchEndTime DATETIME
-        BEGIN TRY
-           SET @batchStartTime = GETDATE();
-            PRINT ('=============================');
-            PRINT ('Loading data into Bronze layer tables')
-            PRINT ('=============================');
-            PRINT ('LOADING CRM TABLES')
-            PRINT ('=============================');
-            PRINT 'Truncating and loading Bronze.crm_cst_info,'
-            SET @startTime = GETDATE();
-            TRUNCATE TABLE Bronze.crm_cst_info
-            BULK INSERT Bronze.crm_cst_info
-                FROM 'C:\Users\kendr\data-analysis\sql-data-warehouse-project\data\datasets\source_crm\cust_info.csv'
-                WITH (FIRSTROW =2, FIELDTERMINATOR =',', TABLOCK )
-
-            TRUNCATE TABLE Bronze.crm_prd_info
-            BULK INSERT Bronze.crm_prd_info
-                FROM 'C:\Users\kendr\data-analysis\sql-data-warehouse-project\data\datasets\source_crm\prd_info.csv'
-                WITH (FIRSTROW =2, FIELDTERMINATOR =',', TABLOCK )
-
-            TRUNCATE TABLE Bronze.crm_sales_details
-            BULK INSERT Bronze.crm_sales_details
-                FROM 'C:\Users\kendr\data-analysis\sql-data-warehouse-project\data\datasets\source_crm\sales_details.csv'
-                WITH (FIRSTROW =2, FIELDTERMINATOR =',', TABLOCK )
-            SET @endtime = GETDATE();
-            PRINT ('Time elapsed: ' + CAST(DATEDIFF(SECOND,@startTime,@endtime) AS NVARCHAR(10)) + ' seconds')
-            PRINT ('=============================');
-
-            PRINT ('LOADING ERP TABLES')
-            SET @startTime = GETDATE();
-            TRUNCATE TABLE Bronze.erp_px_cat_g1v2
-            BULK INSERT Bronze.erp_px_cat_g1v2
-                FROM 'C:\Users\kendr\data-analysis\sql-data-warehouse-project\data\datasets\source_erp\PX_CAT_G1V2.csv'
-                WITH (FIRSTROW =2, FIELDTERMINATOR =',', TABLOCK )
-
-
-            TRUNCATE TABLE Bronze.erp_loc_a101
-            BULK INSERT Bronze.erp_loc_a101
-                FROM 'C:\Users\kendr\data-analysis\sql-data-warehouse-project\data\datasets\source_erp\LOC_A101.csv'
-                WITH (FIRSTROW =2, FIELDTERMINATOR =',', TABLOCK )
-
-            TRUNCATE TABLE Bronze.erp_cust_az12
-            BULK INSERT Bronze.erp_cust_az12
-                FROM 'C:\Users\kendr\data-analysis\sql-data-warehouse-project\data\datasets\source_erp\CUST_AZ12.csv'
-                WITH (FIRSTROW =2, FIELDTERMINATOR =',', TABLOCK )
-            SET @endtime = GETDATE();
-            PRINT ('Time elapsed: ' + CAST(DATEDIFF(SECOND,@startTime,@endtime) AS NVARCHAR(10)) + ' seconds')
-            PRINT ('=============================');
-
-           SET @batchEndTime = GETDATE();
-           PRINT ('Total time for Bronze layer load: ' + CAST(DATEDIFF(SECOND,@batchStartTime,@batchEndTime) AS NVARCHAR(10)) + ' seconds')
-           PRINT ('=============================');
-
-           END TRY
-        BEGIN CATCH
-            PRINT ('=============================');
-            PRINT ('Error occurred while loading data into Bronze layer tables')
-            PRINT 'ERROR ' + CAST(ERROR_NUMBER() AS NVARCHAR(10)) + ': ' + ERROR_MESSAGE()
-            PRINT ('=============================');
-        END CATCH
-END
-
+GO
